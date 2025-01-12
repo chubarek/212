9364f07980d67c7ce74791b7bfcb7669fa8be362
chubarek <haritonovivashka@yandex.ru>
2025-01-12 08:57:08 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+    private String catName;
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(String message) {
+        this.catName = message;
+    }
+
+}
